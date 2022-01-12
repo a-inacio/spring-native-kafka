60ec13b02c12230ac9158d28ee34545e6e34740e
António Inácio <ainacio@neoception.com>
2022-01-12 11:08:51 +0000

    refactor: 💡 Review relevant native hints and document error

--- src/main/java/com/example/springnative/kafka/KafkaApplication.java
+++ src/main/java/com/example/springnative/kafka/KafkaApplication.java
@@ -2,8 +2,6 @@ package com.example.springnative.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaBootstrapConfiguration;
-import org.springframework.kafka.annotation.KafkaListenerConfigurationSelector;
 import org.springframework.nativex.hint.MethodHint;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeHint;
@@ -13,13 +11,28 @@ import org.springframework.nativex.hint.TypeHint;
 //==============================================================================
 // Hint found at https://github.com/spring-projects-experimental/spring-native/blob/main/samples/kafka
 // No effect? Maybe we are breaking before getting to the part where it is needed
-@NativeHint(trigger = KafkaListenerConfigurationSelector.class, types = {@TypeHint(types={KafkaBootstrapConfiguration.class})})
+//@NativeHint(
+//		trigger = org.springframework.kafka.annotation.KafkaListenerConfigurationSelector.class,
+//		types = {@TypeHint(types={org.springframework.kafka.annotation.KafkaBootstrapConfiguration.class})})
+//@NativeHint(options = {"--enable-url-protocols=https"})
 
 // Hints added by trying to fix log messages
-@TypeHint(types={org.apache.kafka.common.security.plain.PlainLoginModule.class})
+@TypeHint(types = {org.apache.kafka.common.security.plain.PlainLoginModule.class})
 @TypeHint(types = {org.apache.kafka.common.security.authenticator.SaslClientCallbackHandler.class}, methods = @MethodHint(name = "<init>"))
 @TypeHint(types = {org.apache.kafka.common.security.authenticator.DefaultLogin.class}, methods = @MethodHint(name = "<init>"))
 @TypeHint(types = {org.apache.kafka.common.security.authenticator.AbstractLogin.DefaultLoginCallbackHandler.class}, methods = @MethodHint(name = "<init>"))
+
+// Added to fix the missing local_US error message
+@NativeHint(options = {"-H:IncludeResourceBundles=sun.security.util.Resources"})
+
+// Trial and Error... no effect in the final error
+//@TypeHint(types = {javax.security.sasl.SaslClient.class}, methods = @MethodHint(name = "<init>"))
+//@TypeHint(types = {javax.security.auth.spi.LoginModule.class}, methods = @MethodHint(name = "<init>"))
+//@TypeHint(types = {org.apache.kafka.common.security.authenticator.SaslClientAuthenticator.class}, methods = @MethodHint(name = "<init>"))
+//@TypeHint(types = {javax.security.sasl.SaslClient.class}, methods = @MethodHint(name = "<init>"))
+//@TypeHint(types = {org.apache.kafka.common.security.scram.internals.ScramSaslClient.class}, methods = @MethodHint(name = "<init>"))
+//@TypeHint(types = {org.apache.kafka.common.security.scram.internals.ScramSaslClient.ScramSaslClientFactory.class}, methods = @MethodHint(name = "<init>"))
+//@TypeHint(types = {org.apache.kafka.common.security.scram.ScramLoginModule.class}, methods = @MethodHint(name = "<init>"))
 //==============================================================================
 
 @SpringBootApplication
